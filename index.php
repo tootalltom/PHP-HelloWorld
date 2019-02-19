29b68e2d5568b0e44b35ce0533c2f78520788e2d
tootalltom <45954030+tootalltom@users.noreply.github.com>
2019-02-19 11:33:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?>
+ <?php echo '<p>Hello World-1. This is my first page using OpenShift Online</p>'; ?>
  </body>
 </html> 
